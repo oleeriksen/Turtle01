84fc09fee4201aad63c5a18cc7c24994d438aba5
oleeriksen <oleeriksen1958@gmail.com>
2024-02-05 11:47:10 +0100

    Update ITurtle.cs

--- WebApp/Model/ITurtle.cs
+++ WebApp/Model/ITurtle.cs
@@ -3,13 +3,15 @@
     public interface ITurtle
     {
         void Down();
+        void Up();
         void Move(double distance);
         void MoveTo(double x, double y);
-        void Reset();
+        
         void Turn(double d);
         void TurnTo(double d);
-        void Up();
         string Color { get; set; }
         double Width { get; set; }
+
+        void Reset();
     }
 }
