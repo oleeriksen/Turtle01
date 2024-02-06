15098be134db8dc658baa554981d385d60b577ea
oleeriksen <oe@easv.dk>
2024-02-06 10:59:29 +0100

    div

---
+++ WebApp/Drawings/StarDrawing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using WebApp.Model;
+
+namespace WebApp.Drawings
+{
+	public class StarDrawing
+	{
+		public StarDrawing()
+		{
+		}
+
+        public void DrawStar(ITurtle t, int n)
+        {
+            double centerX = 250, centerY = 150;
+            t.Up();
+            t.Color = "red";
+            t.MoveTo(centerX, centerY);
+            t.TurnTo(0);
+            t.Down();
+
+            for (int i = 0; i < n; i++)
+            {
+                t.Move(100); // her tegnes
+                t.Up();
+                t.MoveTo(centerX, centerY);
+                t.Turn(360 / n);
+                t.Down();
+            }
+
+        }
+    }
+}
+
---
+++ WebApp/Drawings/TriangleDrawing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WebApp.Model;
+
+namespace WebApp.Drawings
+{
+	public class TriangleDrawing
+	{
+		public TriangleDrawing()
+		{
+		}
+
+        public void DrawTriangle(ITurtle t, double width, int level)
+        {
+            if (level == 0)
+                t.Move(width);
+            else
+            {
+                var nw = width / 3;
+                DrawTriangle(t, nw, level - 1);
+                t.Turn(-60);
+                DrawTriangle(t, nw, level - 1);
+                t.Turn(120);
+                DrawTriangle(t, nw, level - 1);
+                t.Turn(-60);
+                DrawTriangle(t, nw, level - 1);
+            }
+        }
+    }
+}
+
