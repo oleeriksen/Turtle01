2bb71b3f9b2c6e8ecc2dff5eb0937e14125f6eb4
oleeriksen <oleeriksen1958@gmail.com>
2024-02-21 11:55:12 +0100

    random

---
+++ WebApp/Drawings/RandomDrawing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WebApp.Model;
+namespace WebApp.Drawings
+{
+    public class RandomDrawing
+    {
+
+        public void DrawRandom(ITurtle t, int steps) {
+            t.Up();
+            t.MoveTo(500, 300);
+            t.Down();
+            t.Move(100);
+
+        }
+
+    }
+}
+
