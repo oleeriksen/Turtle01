0796acf74c84a84c6d12687e37cf294e17158ddb
oleeriksen <oleeriksen1958@gmail.com>
2024-08-26 13:35:28 +0200

    Update StarDrawing.cs

--- WebApp/Drawings/StarDrawing.cs
+++ WebApp/Drawings/StarDrawing.cs
@@ -5,32 +5,8 @@ namespace WebApp.Drawings
 {
 	public class StarDrawing
 	{
-        /// <summary>
-        /// Draw [n] stars of random size and location using
-        /// the turtle [t]
-        /// </summary>
-        /// <param name="t">The turtle to draw with</param>
-        /// <param name="n">The number of stars</param>
-        public void DrawManyStars(ITurtle t, int n)
-        {
-            Random r = new Random();
-            t.Width = 1;
-
-            for (int i = 0; i < n; i++)
-            {
-                int size = 50 + r.Next(100);
-                int cx = size + r.Next(1000 - 2*size);
-                int cy = size + r.Next(600 - 2*size);
-                
-                // to-do: overfør size, cx og cy som parametre til DrawOneStar
-                DrawOneStar(t);
-            }
-
-        }
-
         public void DrawOneStar(ITurtle t)
         {
-
             double size = 75; 
             double cx = 500; 
             double cy = 300;
@@ -51,8 +27,28 @@ namespace WebApp.Drawings
                 t.Down();
             }
         }
-
         
+        /// <summary>
+        /// Draw [n] stars of random size and location using
+        /// the turtle [t]
+        /// </summary>
+        /// <param name="t">The turtle to draw with</param>
+        /// <param name="n">The number of stars</param>
+        public void DrawManyStars(ITurtle t, int n)
+        {
+            Random r = new Random();
+            t.Width = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int size = 50 + r.Next(100);
+                int cx = size + r.Next(1000 - 2*size);
+                int cy = size + r.Next(600 - 2*size);
+                
+                // to-do: overfør size, cx og cy som parametre til DrawOneStar
+                DrawOneStar(t);
+            }
+        }
     }
 }
 
